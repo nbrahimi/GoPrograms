f6b64cef8f66583a65b7c9bf27dbaadc7f80699f
NoureddineB <noureddinebrahimi@yahoo.ca>
2025-04-20 19:12:27 -0400

    Add files via upload

--- Chap1/main.go
+++ Chap1/main.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		w := new(app.Window)
+		th := material.NewTheme()
+		var ops op.Ops
+		for {
+			switch e := w.Event().(type) {
+			case app.DestroyEvent:
+				os.Exit(0)
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
+				material.Label(th, 16, "Hello, World!").Layout(gtx)
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
+	app.Main()
+}
