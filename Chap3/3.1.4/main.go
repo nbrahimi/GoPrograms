f443797128e31c113628914dd8451252e10719e6
NoureddineB <noureddinebrahimi@yahoo.ca>
2025-07-31 21:45:13 -0400

    Add files via upload

--- Chap3/3.1.4/main.go
+++ Chap3/3.1.4/main.go
@@ -1,1 +1,43 @@
+package main
 
+import (
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		w := new(app.Window)
+		w.Option(app.Title("3.1.4-Rigid vs Flexed"))
+		w.Option(app.Size(unit.Dp(400), unit.Dp(600)))
+		th := material.NewTheme()
+		var ops op.Ops
+		var rigidButton, flexedButton widget.Clickable
+		for {
+			switch e := w.Event().(type) {
+			case app.DestroyEvent:
+				os.Exit(0)
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
+				layout.Flex{
+					Axis: layout.Horizontal, // Arrange labels in a row
+				}.Layout(gtx,
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						return material.Button(th, &rigidButton, "Rigid").Layout(gtx)
+					}),
+					layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+						return material.Button(th, &flexedButton, "Expandable").Layout(gtx)
+					}),
+				)
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
+	app.Main()
+}
