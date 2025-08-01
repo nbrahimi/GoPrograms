7d3186dc4625eb659b5d073543efe434d5b6499f
NoureddineB <noureddinebrahimi@yahoo.ca>
2025-07-31 21:44:29 -0400

    Add files via upload

--- Chap3/3.1.3/main.go
+++ Chap3/3.1.3/main.go
@@ -1,1 +1,33 @@
+package main
 
+import (
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		w := new(app.Window)
+		w.Option(app.Title("3.1.3"))
+		w.Option(app.Size(unit.Dp(400), unit.Dp(600)))
+		th := material.NewTheme()
+		var ops op.Ops
+		var clickme widget.Clickable
+		for {
+			switch e := w.Event().(type) {
+			case app.DestroyEvent:
+				os.Exit(0)
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
+				material.Button(th, &clickme, "Click me!").Layout(gtx)
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
+	app.Main()
+}
