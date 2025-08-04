b463d5e66323ac4e628ce6114c2f2e4dcc5b3363
NoureddineB <noureddinebrahimi@yahoo.ca>
2025-08-03 21:49:56 -0400

    Add files via upload

--- Chap3/3.2.1/main.go
+++ Chap3/3.2.1/main.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+	"fmt"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		w := new(app.Window)
+		w.Option(app.Title("Scrollable list"))
+		w.Option(app.Size(unit.Dp(400), unit.Dp(600)))
+		th := material.NewTheme()
+		var ops op.Ops
+		for {
+			switch e := w.Event().(type) {
+			case app.DestroyEvent:
+				os.Exit(0)
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
+				list := layout.List{Axis: layout.Vertical}
+				list.Layout(gtx, 10, func(gtx layout.Context, index int) layout.Dimensions {
+					return material.Label(th, unit.Sp(20), fmt.Sprintf("Item %d", index+1)).Layout(gtx)
+				})
+
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
+	app.Main()
+}
