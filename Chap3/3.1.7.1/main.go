9022e322b6d524dfa8653f001835e5d8ac828f57
NoureddineB <noureddinebrahimi@yahoo.ca>
2025-08-03 21:43:14 -0400

    Add files via upload

--- Chap3/3.1.7.1/main.go
+++ Chap3/3.1.7.1/main.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		w := new(app.Window)
+		w.Option(app.Title("3.1.7.1-layout.Inset"))
+		w.Option(app.Size(unit.Dp(400), unit.Dp(600)))
+		th := material.NewTheme()
+		var btn widget.Clickable
+		var ops op.Ops
+		margins := layout.Inset{Top: unit.Dp(10), Left: unit.Dp(20)}
+		for {
+			switch e := w.Event().(type) {
+			case app.DestroyEvent:
+				os.Exit(0)
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
+				margins.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+					return material.Button(th, &btn, "Click me!").Layout(gtx)
+				})
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
+	app.Main()
+}
