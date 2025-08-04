173355a7a4ca1945773ee675a4fb13cd426feb44
NoureddineB <noureddinebrahimi@yahoo.ca>
2025-08-04 12:27:20 -0400

    Add files via upload

--- Chap4/4.1.1/main.go
+++ Chap4/4.1.1/main.go
@@ -1,1 +1,49 @@
+package main
 
+import (
+	"log"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		w := new(app.Window)
+		w.Option(app.Title("Editor"))
+		w.Option(app.Size(unit.Dp(400), unit.Dp(600)))
+		if err := run(w); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	app.Main()
+}
+
+func run(w *app.Window) error {
+	th := material.NewTheme()
+	var ops op.Ops
+	var editor widget.Editor
+	editor.SingleLine = true // Set single-line mode
+
+	for {
+		switch e := w.Event().(type) {
+		case app.DestroyEvent:
+			os.Exit(0)
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+			// UI Layout
+			layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+				return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+					layout.Rigid(material.Editor(th, &editor, "Type here...").Layout), // Input field
+					layout.Rigid(material.Body1(th, "Copy: "+editor.Text()).Layout),   // Non-editable field. Contain a copy of the previous
+				)
+			})
+			e.Frame(gtx.Ops)
+		}
+	}
+}
